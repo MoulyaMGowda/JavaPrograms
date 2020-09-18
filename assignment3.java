042587c6699ea8d99b528e16a3d6fc28f3540ca1
MoulyaMGowda <71440732+MoulyaMGowda@users.noreply.github.com>
2020-09-18 10:15:53 +0530

    Add files via upload

---
+++ assignment3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class assignment3 {
+        public static void main(String[] args){
+
+            int maxNumber = 20;
+            int previousNumber = 0;
+            int nextNumber = 1;
+            int i = 0;
+            System.out.print("Fibonacci Series of "+maxNumber+" numbers:");
+
+            while ( i <= maxNumber)
+            {
+                System.out.print(previousNumber+" ");
+                int sum = previousNumber + nextNumber;
+                previousNumber = nextNumber;
+                nextNumber = sum;
+                ++i;
+            }
+        }
+}
